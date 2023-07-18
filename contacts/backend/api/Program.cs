b9793f44139d03a72dee1be203a9bffcda3aba75
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-07-18 19:42:53 +0200

    refactor: remove comments

--- contacts/backend/api/Program.cs
+++ contacts/backend/api/Program.cs
@@ -5,8 +5,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddDbContext<ContactsDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("ContactsDb"));
@@ -33,13 +31,11 @@ builder.Services.AddControllers().AddNewtonsoftJson(options =>
     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
 });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
