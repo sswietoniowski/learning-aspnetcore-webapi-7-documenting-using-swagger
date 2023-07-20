5dc481776a99d2a711c775ed4dbe5755dd668742
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-07-20 22:46:36 +0200

    refactor: code refactored

--- contacts/backend/api/DTOs/ContactWithPhonesForCreationDto.cs
+++ contacts/backend/api/DTOs/ContactWithPhonesForCreationDto.cs
@@ -5,5 +5,5 @@ namespace Contacts.Api.DTOs;
 public class ContactWithPhonesForCreationDto : ContactForCreationDto
 {
     [Required]
-    public ICollection<PhoneForCreationDto> Phones { get; set; } = new List<PhoneForCreationDto>();
+    public List<PhoneForCreationDto> Phones { get; set; } = new();
 }
